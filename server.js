6898450aa6258cb7656a32d818b3bdbde872cf51
Piotr_Wróbel_3I1a <wrobelpiotr24@gmail.com>
2022-01-04 17:19:36 +0100

    commit kolejna zmiana w pliku!

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", function (req, res) {
     res.send("<h1>my app on heroku - kolejna zmiana w pliku</h1>")
 })
 
+app.get("/data", function (req, res) {
+    res.send('<h1>imie: "Piotr", nazwisko: "Wróbel", klasa: "3I1", grupa: "a"</h1>')
+})
+
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
