2a81cc30ed52ea08fcc85b2bb1f3898ac15fdadc
Piotr_Wróbel_3I1a <wrobelpiotr24@gmail.com>
2022-01-04 17:29:21 +0100

    commit kolejna zmiana w pliku!

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-    res.send("<h1>my app on heroku - kolejna zmiana w pliku!!!</h1>")
+    res.send("<h1>my app on heroku - kolejna zmiana w pliku!</h1>")
 })
 
 app.get("/data", function (req, res) {
